42c665d7ee0a00a82edb8bd6294417ddd5e80fa6
Idan Rakovsky <idanr98@gmail.com>
2020-03-18 22:04:05 +0200

    first GET example Route

--- corona-service.js
+++ corona-service.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var app = express();
 
-app.get("/url", (req, res, next) => {
+app.get("/v1/labs", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(8200, () => {
+ console.log("Server running on port 8200");
 });
