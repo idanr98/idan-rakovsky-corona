0e9132bf2a675fadc2c076f060f7ecc00a4ba578
Idan Rakovsky <idanr98@gmail.com>
2020-03-19 13:24:55 +0200

    status code works!!

--- corona-service.js
+++ corona-service.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/v1/labs", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]).sendStatus(200);
+    res.status(200).json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
 app.listen(8200, () => {
