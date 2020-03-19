d0dcdb3750ebbde2935dff4cf598da7fe0460a0c
Idan Rakovsky <idanr98@gmail.com>
2020-03-19 12:37:57 +0200

    not working add status-code

--- corona-service.js
+++ corona-service.js
@@ -2,9 +2,10 @@ var express = require("express");
 var app = express();
 
 app.get("/v1/labs", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]).sendStatus(200);
    });
 
 app.listen(8200, () => {
  console.log("Server running on port 8200");
 });
+
