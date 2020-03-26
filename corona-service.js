0f63929c1d89f54e24bf49fb7e6af47bee53ee9b
Idan Rakovsky <idanr98@gmail.com>
2020-03-26 10:25:56 +0200

    add some js code and the cure button

--- corona-service.js
+++ corona-service.js
@@ -1,15 +1,36 @@
 var express = require("express");
 var app = express();
 
-app.get("/v1/labs", (req, res, next) => {
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var classname = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', animateButton, false);
+    
+  }
+
+
+
+app.get("/api/v1/labs", (req, res, next) => {
     res.status(200).json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
-app.get("/v1/labs", (req, res, next) => {
+app.get("/api/v1/soldiers", (req, res, next) => {
     res.status(200).json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
-app.get("/", (req, res, next) => {
+app.get("/api/v1/cure/found", (req, res, next) => {
     res.status(200).json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
